aee630a1637a1eda4ae5e95e6aea2ff1fb0e9b2f
TuomasSimula <tuomas.e.simula@gmail.com>
2018-11-17 22:43:58 +0200

    Pelaajan liikkuminen
    Kirjoitin pelaajalle skriptin jolla se liikkuu, säädin animaatioiden kanssa

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,46 +4,72 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float moveSpeed;
+    public float moveSpeed = 1;
+    public float runMultiplier = 2;
+    public float delay = 0.1f;
 
     private Animator anim;
+    private Rigidbody2D rb;
 
     private bool playerMoving;
     private Vector2 lastMove;
     private bool playerRunning;
+    private float t = 0.0f;
 
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
         playerMoving = false;
         playerRunning = false;
 
-		if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f )
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        Vector3 playerVelocity = Vector3.zero;
+
+        if (Time.time < t + delay && Time.time > delay)
         {
-            transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            playerVelocity = new Vector3(lastMove.x, lastMove.y, 0.0f);
             playerMoving = true;
-            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            anim.SetFloat("MoveX", lastMove.x);
+            anim.SetFloat("MoveY", lastMove.y);
         }
-
-        if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f )
+        else
         {
-            transform.Translate (new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-            playerMoving = true;
-            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            if (moveX != 0.0f || moveY != 0.0f)
+            {
+                playerVelocity = new Vector3(moveX, moveY, 0.0f);
+                playerMoving = true;
+                lastMove = new Vector2(moveX, moveY);
+            }
+
+            playerVelocity *= moveSpeed;
+
+            if (Input.GetAxisRaw("Run") != 0.0f)
+            {
+                playerRunning = true;
+                playerVelocity *= runMultiplier;
+            }
+
+            if (moveX != 0 && moveY != 0)
+            {
+                playerVelocity /= 1.4142f;
+                t = Time.time;
+            }
+
+            anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+            anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
         }
 
-        if (Input.GetAxisRaw("Run") > 0.5f || Input.GetAxisRaw("Run") < -0.5f)
-        {
-            playerRunning = true;
-        }
+        if (playerVelocity != Vector3.zero)
+            lastMove = (Vector2) playerVelocity;
 
-        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
-        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        rb.velocity = playerVelocity;
+
         anim.SetBool("PlayerMoving", playerMoving);
         anim.SetBool("PlayerRunning", playerRunning);
         anim.SetFloat("LastMoveX", lastMove.x);
