21be5f9b8f2ff84c7e79db44c1cf4ac17f35480e
TuomasSimula <tuomas.e.simula@gmail.com>
2018-11-19 20:21:25 +0200

    Kysyvä NPC
    NPC esittää kysymyksen, johon voi valita vastauksen numeronäppäimillä

--- Assets/Scripts/ChoicesNPC.cs
+++ Assets/Scripts/ChoicesNPC.cs
@@ -3,19 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TalkingNPC : MonoBehaviour {
-
+public class ChoicesNPC : MonoBehaviour
+{
     public Text message;
 
     private bool playerIsInTrigger = false;
+    private bool askedQuestion = false;
 
-    void Update ()
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && playerIsInTrigger)
         {
-            message.text = "Hello there!";
+            message.text = "Choose an answer\n(1) answer 1\n(2) answer 2";
+            askedQuestion = true;
         }
-	}
+
+        if (Input.GetKeyDown(KeyCode.Alpha1) && askedQuestion && playerIsInTrigger)
+        {
+            message.text = "You answered 1";
+            askedQuestion = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2) && askedQuestion && playerIsInTrigger)
+        {
+            message.text = "You answered 2";
+            askedQuestion = false;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
